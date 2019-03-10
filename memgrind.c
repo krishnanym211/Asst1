9c4aa93336f85895019ebc7fc196e88776b30ac6
Nightwingbeyond <30642762+Nightwingbeyond@users.noreply.github.com>
2019-03-09 23:54:56 -0500

    a & b are done, c is 50%, changed header file a bit

--- memgrind.c
+++ memgrind.c
@@ -1,16 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
 
 #include "mymalloc.h"
 
+//malloc() 1 byte and immediately free it - do this 150 times
 void testA(){
-	return;
+    int count = 0;
+    int total = 0;
+    while (count < 100){
+        clock_t start = clock(), diff;
+        int i;
+        for(i = 0;i < 150; i++){
+            char * input = (char *) malloc(sizeof(char));
+            free(input);
+        }
+        diff = clock() - start;
+        total += diff;
+        count++;
+    }
+    return total/100;
 }
+
+//malloc() 1 byte, store the pointer in an array - do this 150 times.
+//Once you've malloc()ed 50 byte chunks, then free() the 50 1 byte pointers one by one.
 void testB(){
-	return;
+    int count = 0;
+    int total = 0;
+    while (count < 100){
+        clock_t start = clock(), diff;
+        char * inputs[150];
+        int i;
+        for (i = 0;i<150;i++){
+            inputs[i] = (char *) malloc(1);
+            if (inputs[i] == NULL){
+                return -1;
+            }
+        }
+        int j;
+        for (j = 0;j<150;j++){
+            free(inputs[j]);
+        }
+        diff = clock() - start;
+        total+=diff;
+        count++;
+    }
+    return total/100;
 }
+
+//Randomly choose between a 1 byte malloc() or free()ing a 1 byte pointer
 void testC(){
-	return;
+    int count = 0;
+    int total = 0;
+    srand(time(NULL));
+    int mallocCounter = 0;
+    char* input[4096];
+    while (count < 100){
+        clock_t start = clock(), diff;
+        
+        int r = rand()%2;
+        int mallocindex = 0;
+        int freeindex = 0;
+        while(mallocCounter < 50){
+            if(r == 0){
+                //perform malloc
+                
+                mallocCounter++;
+            }else{
+                //perform free
+                
+            }
+        }
+    }
+    return total/100;
 }
 void testD(){
 	return;
